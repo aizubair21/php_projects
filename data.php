357442bf7c2ba55be504160c80fc2917c8936d94
amimul21 <amimulit21@gmail.com>
2021-10-06 19:36:04 +0600

    add multidimentional array, and associated array

--- data.php
+++ data.php
@@ -64,23 +64,53 @@
 
 
     $student_into = [
-      $zakir = [
-      "name"=>"zakir",
-      "village"=>"pabna",
-      "id"=>uniqid(10),
-      "dipt"=>"DIS",
-      ],
-      $zubair = [
+      "zubair" => [
         "name"=>"zubair",
-        "village"=>"Bhola",
-        "id"=>uniqid(10),
-        "dipt"=>"DIT",
+        "class" => "Inter 1st",
+        "id"=>"ABC123",
+        "address" => [
+          "p_addr" => [
+            "district" => "Bhola",
+            "sub_dist" => "lalmohan",
+            "post" => "lahordhi",
+            "village" => "mayakanon",
+            "post_code" => "2540",
+          ],
+          "c_addr" => [
+            "district" => "Kushtia",
+            "sub_dist" => "shoilkupa",
+            "post" => "shoilkupa",
+            "village" => "modhukhali",
+            "post_code" => "1540",
+          ],
+        ],
+        "student_query" => [
+          "academic" => "Brilliant", "result" => "Pass",
+        ],
       ],
-      $Rayhan = [
+      "Rayhan" => [
         "name"=>"Rayhan",
-        "village"=> "lord",
-        "id"=>uniqid(10),
-        "dipt"=>"DIT",
+        "class"=>"inter 2nd",
+        "id"=> "ABC234",
+        "address" => [
+          "p_addr" => [
+            "district" => "Pabns",
+            "sub_dist" => "kanaipur",
+            "post" => "sadhu khali",
+            "village" => "mayakanon",
+            "post_code" => "2540",
+          ],
+          "c_addr" => [
+            "district" => "Jhenidhah",
+            "sub_dist" => "shoilkupa",
+            "post" => "sheikhpara",
+            "village" => "modhupur",
+            "post_code" => "1540",
+          ],
+        ],
+        "student_query" => [
+          "academic" => "Brilliant", "result" => "Pass",
+        ],
       ],
     ];
     
@@ -92,17 +122,60 @@ echo '
     <table border="1">
       <tr>
         <th>Name</th>
-        <th>Adr</th>
+        <th>Class</th>
         <th>ID</th>
-        <th>dipt</th>
+        <th>Address
+          <table border="1">
+            <tr>
+              <th>Dist</th>
+              <th>Sub_Dist</th>
+              <th>Post</th>
+              <th>Village</th>
+              <th>Post_Code</th>
+            </tr>
+            <tr>
+
+            </tr>
+          </table>
+        </th>
+        <th> Student Querry
+          <table border="1">
+            <tr>
+              <th>Academic</th>
+              <th>Result</th>
+            </tr>
+          </table>
+        </th>
       </tr>';
        foreach ($student_into as $students) {
+          print_r($students);
       echo '
       <tr>
-        <td>' .$students["name"]. '</td>
-        <td>' .$students["village"].'</td>
-        <td>' .$students["id"] . '</td>
-        <td>' .$students["dipt"].' </td>
+        <th>'.$students["name"].'</th>
+        <th>'.$students["class"].'</th>
+        <th>'.$students["id"].'</th>
+        <th>
+          <table border="1">
+            <tr>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+            </tr>
+            <tr>
+
+            </tr>
+          </table>
+        </th>
+        <th>  
+          <table border="1">
+            <tr>
+              <th></th>
+              <th></th>
+            </tr>
+          </table>
+        </th>
       </tr>';
   
       };
@@ -110,121 +183,3 @@ echo '
     // print_r($input_name);
     
   ?>
-
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" referrerpolicy="no-referrer"></script>
- 
-    <style>
-      /*style hone here*/  
-    li {
-      list-style-type: none;
-    }
-    /* .main_h {
-      position: relative;
-    }
-    .main_h::before {
-      content: "";
-      position: absolute;
-      top: 5px;
-      left: -5px;
-      transform: translate(-50%, -50%);
-      width: 5px;
-      height: 5px;
-      background-color: green;
-      border-radius: 5px;
-    } */
-
-
-.main_h {
-  cursor: pointer;
-  -webkit-user-select: none; /* Safari 3.1+ */
-  -moz-user-select: none; /* Firefox 2+ */
-  -ms-user-select: none; /* IE 10+ */
-
-}
-
-.main_h::before {
-  /* content: "\25B8"; */
-  content: "\229E";
-  color: black;
-  display: inline-block;
-  margin-right: 6px;
-}
-.caret-down::before {
-  content: "\229F";
-  color: green;
-
-}
-
-.child {
-  display: none;
-}
-
-.active {
-  display: block;
-  animation: all linear .5s;
-}
-    </style>
-  </head>
-
-  <body>
-    <ul style="background-color: gray; width:250px">
-      <li  > <span class="main_h">Zubair</span>
-        <ul class="child">
-          <li>Name :</li>
-          <li>class :</li>
-          <li>ID :</li>
-           <li >
-              <span class="main_h">P_addr</span> :
-              <ul class="child">
-                <li>Dist :</li>
-                <li>Sub_dist :</li>
-                <li>Post :</li>
-                <li>Village :</li>
-                <li>Post Code :</li>
-              </ul>
-           </li>
-        </ul>
-      </li>
-      <li > <span class="main_h">Rayhan</span>
-        <ul class="child">
-          <li>Name: </li>
-          <li> Class :</li>
-          <li> ID :</li>
-          <li > <span class="main_h">P_addr</span>
-            <ol class="child">
-              <li>Dist :</li>
-              <li>Sub_dist :</li>
-              <li>Post :</li>
-              <li>Village :</li>
-              <li>Post Code :</li>
-            </ol>
-          </li>
-        </ul>
-      </li>
-    </ul>
-  </body>
-
-  <script>
-
-    //js gone here
-    var toggler = document.getElementsByClassName("main_h");
-    var i;
-
-    for (i = 0; i < toggler.length; i++) {
-      toggler[i].addEventListener("click", function() {
-        this.parentElement.querySelector(".child").classList.toggle("active");
-        this.classList.toggle("caret-down");
-        // console.log(this);
-        
-      });
-    }
-
-  </script>
-  </html>
